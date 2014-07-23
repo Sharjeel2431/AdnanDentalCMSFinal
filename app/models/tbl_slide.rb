35bc5fc834359c1474532112f772f1feb1892dee
Sharjeel Ahmad Qureshi <sharjeelahmadqureshi@outlook.com>
2014-07-23 11:29:59 +0500

    asdasd

--- app/models/tbl_slide.rb
+++ app/models/tbl_slide.rb
@@ -1,4 +1,4 @@
 class TblSlide < ActiveRecord::Base
-  has_attached_file :avatar, styles: { small: "340x170", medium: "740x440", large: "1440x740" }
+  has_attached_file :avatar, styles: { small: "240x130", medium: "740x440", large: "1440x740" }
 
 end
